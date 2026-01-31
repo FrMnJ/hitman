bddb90a5a9949e702d028f3c9fe8860fc0b5a9ee
FrMnJ <manuelserratossec38@gmail.com>
2026-01-31 16:07:02 -0600

    Refactor extract parse port to function

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -7,16 +7,20 @@ import (
 
 const PORT string = "-p"
 
+func ParsePort(args []string) (int, error) {
+	port := args[2]
+	portNum, err := strconv.ParseInt(port, 10, 64)
+	if err != nil {
+		return -1, err
+	}
+	return int(portNum), nil
+}
+
 func ParseArgs(args []string) (int, error) {
 	option := args[1]
 	switch option {
 	case PORT:
-		port := args[2]
-		portNum, err := strconv.ParseInt(port, 10, 64)
-		if err != nil{
-			return -1, err
-		}
-		return int(portNum), nil
+		return ParsePort(args)
 	default:
 		return -1, fmt.Errorf("%s is not a valid option", option)
 	}
