6eb65a0ecaf5c4bba0ce9e0b5be94448241aeb80
FrMnJ <manuelserratossec38@gmail.com>
2026-01-31 16:34:47 -0600

    Refactor SpawnProcOnPort to use constants for port validation

--- internal/proc/proc.go
+++ internal/proc/proc.go
@@ -2,14 +2,20 @@ package proc
 
 import "errors"
 
+const (
+	MaxPort int = 65535
+	MinPort int = 0
+	InvalidPID int = -1
+)
+
 var (
 	ErrInvalidPort error = errors.New("Invalid port range 0 to 65535")
 	NotImplemented error = errors.New("Not Implemented")
 )
 
 func SpawnProcOnPort(port int) (pid int, err error) {
-	if port > 65535 || port < 0 {
-		return -1, ErrInvalidPort
+	if port > MaxPort || port < MinPort {
+		return InvalidPID, ErrInvalidPort
 	}
-	return -1, NotImplemented
+	return InvalidPID, NotImplemented
 }
